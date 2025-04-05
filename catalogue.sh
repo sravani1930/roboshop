faea123c4236612d75001f81f78964e2e649b9e7
sravani192019 <sravani.jamithireddy@gmail.com>
2025-04-05 14:59:20 +0530

    catalogue

--- catalogue.sh
+++ catalogue.sh
@@ -14,10 +14,10 @@ N="\e[0m"
 validate(){
    if [ $1 -ne 0 ]
    then
-      echo -e "$R ERROR :: $2 .. FAILED $N"
+      echo -e "ERROR :: $2 .. $R FAILED $N"
       exit 1;
    else
-      echo -e "$G $2 ... SUCCESS $N"
+      echo -e "$2 ... $G SUCCESS $N"
    fi
 }
 
